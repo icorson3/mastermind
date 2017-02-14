5e5650c4f6e3b6d5ac4dcfca89a72b942577e2b4
Ilana Corson <ilana.corson@gmail.com>
2017-02-14 09:02:16 -0700

    starting point

--- mastermind.rb
+++ mastermind.rb
@@ -1,26 +1,65 @@
-require_relative 'game'
-require_relative 'messages'
 
 class Mastermind
-  include Messages
 
   def initialize
     player_options
   end
 
   def player_options
-    Messages.welcome
+    puts "Welcome to MASTERMIND\nWould you like to (p)lay, read the (i)nstructions, or (q)uit?"
     input = gets.chomp.downcase
     if input == "p" || input == "play"
-      Game.new.play_game
+      key = %w{r r r r g g g g b b b b y y y y}.sample(4)
+      play_game(key)
     elsif input == "i" || input == "instructions"
-      Messages.instructions
+      puts "these are the instructions"
       player_options
     elsif input == "q" || input == "quit"
-      Messages.quit
+      puts "Thanks for playing!"
       exit
     end
   end
+
+
+def play_game(key)
+  puts "I have generated a beginner sequence with four elements made up of: (r)ed,
+  (g)reen, (b)lue, and (y)ellow. Use (q)uit at any time to end the game.\nWhat's your guess? You can get the secret code by typing (c)heat."
+  guess = gets.chomp.downcase.chars
+  if guess.join == "c" || guess.join == "cheat"
+    puts "You cheater! The secret code is #{key.join}"
+    play_game(key)
+  elsif guess.join == "q" || guess.join == "quit"
+    puts "Thanks for playing!"
+    exit
+  elsif guess.length > 4
+    puts "Your guess is too long. Try again"
+    play_game(key)
+  elsif guess.length < 4
+    puts "Your guess is too short. Try again"
+    play_game(key)
+  else
+    check_letters_for_code_match(guess, key)
+  end
 end
 
-Mastermind.new
+  def check_letters_for_code_match(guess, key)
+    if guess == key
+      puts "You got the write answer!"
+      exit
+    end
+    elements = guess.zip(key).count do |g|
+                g[0] == g[1]
+              end
+    positions = guess.uniq.count do |g|
+                  key.include?(g)
+                end
+    puts "You got #{elements} correct elements in #{positions} correct positions"
+    play_game(key)
+  end
+end
+
+
+
+if __FILE__ == $0
+  Mastermind.new
+end
